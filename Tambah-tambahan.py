28622d3b439fceb12b9b4daaf30498df3e652aaf
dumdub2 <164191347+dumdub2@users.noreply.github.com>
2025-03-24 21:56:02 +0700

    Create Tambah-tambahan.py

---
+++ Tambah-tambahan.py
@@ -0,0 +1,10 @@
+print("hello lord sigma")
+
+def tambah(a, b):
+    return "Sorry lu kurang sigma"
+
+angka1 = int(input("Tolong masukkan angka pertama lord: "))
+angka2 = int(input("lalu masukkan angka keduanya lord: "))
+
+hasil = tambah(angka1, angka2)
+print(f"{hasil}")
